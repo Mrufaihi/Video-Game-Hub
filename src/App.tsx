2ada3a405ed323531dd5b096ef8bff150f1f3ae2
Mrufaihi <113942663+Mrufaihi@users.noreply.github.com>
2024-09-10 13:23:57 +0300

    Resposive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import { Button } from '@chakra-ui/react';
+import { Button, Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `'nav''main' `, //mobile is our base
+        lg: `'nav nav' 'aside main' `, //large devices 1024px +
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      {/* Show only when Above Large */}
+      <Show above="lg">
+        <GridItem area="aside" bg="green">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="gold">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
